2b44f9041da8348417dc75555c7015030aff1817
FrogdanM <bogdan_58@yahoo.com>
2025-04-08 16:47:32 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,36 @@
+activities = ["activity1",
+              "activity2",
+              "activity3",
+              "activity4",
+              "activity5",
+              "activity6",
+              "activity7",
+              "activity8",
+              "activity9",
+              "activity10",
+              "activity11",
+              "activity12"]
+
+attendance_sheets = [
+        "Catalog - January 2025",
+        "Catalog - February 2025",
+        "Catalog - March 2025",
+        "Catalog - April 2025"
+]
+
+
+register1 ={
+        "path":"C:\\file_path\\file.xlsx"
+        "ID":"ID_Column_name",
+        "monthly_attendance":attendance_sheets,
+        "activities":activities,
+        "city": "Bucharest"
+}
+
+register2 ={
+        "path":"C:\\file_path2\\file2.xlsx"
+        "ID":"ID_Column_name",
+        "monthly_attendance":attendance_sheets,
+        "activities":activities,
+        "city": "Bucharest"
+}
