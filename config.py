4e7e5c4565763d3d20cb7838d710ababf9d9ee38
FrogdanM <bogdan_58@yahoo.com>
2025-04-09 17:28:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,36 +1,31 @@
-activities = ["activity1",
-              "activity2",
-              "activity3",
-              "activity4",
-              "activity5",
-              "activity6",
-              "activity7",
-              "activity8",
-              "activity9",
-              "activity10",
-              "activity11",
-              "activity12"]
+activitati = [  "Activity1",
+                "Activity2",
+                "Activity3",
+                "Activity4",
+                "Activity5",
+                "Activity6",
+                "Activity7",
+                "Activity8",
+                "Activity9",
+                "Activity10",
+                "Activity11",
+                "Activity12",
+                "Activity13"]
 
-attendance_sheets = [
-        "Catalog - January 2025",
-        "Catalog - February 2025",
-        "Catalog - March 2025",
-        "Catalog - April 2025"
+cataloage = [
+        "Catalog - Ianuarie 2025",
+        "Catalog - Februarie 2025",
+        "Catalog - Martie 2025",
+        "Catalog - Aprilie 2025"
 ]
 
-
-register1 ={
-        "path":"C:\\file_path\\file.xlsx"
-        "ID":"ID_Column_name",
-        "monthly_attendance":attendance_sheets,
-        "activities":activities,
-        "city": "Bucharest"
-}
-
-register2 ={
-        "path":"C:\\file_path2\\file2.xlsx"
-        "ID":"ID_Column_name",
-        "monthly_attendance":attendance_sheets,
-        "activities":activities,
-        "city": "Bucharest"
-}
+register1 = {
+        "path":"path\\file.xlsx",
+        "nume":"Nume/prenume", 
+        "gen": "Dezagregare Gen si Varsta (W=woman, M=man, g=girl, b=boy)",
+        "reg":"Registru Activitati",
+        "registru" : "",
+        "cataloage":cataloage,
+        "activitati":activitati,
+        "oras": "Bucuresti"
+      }
